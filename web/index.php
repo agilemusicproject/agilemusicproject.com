1723a672df080c201fbd15cf81f15306fc0ea180
chrisdlangham <chris.langham32@gmail.com>
2014-05-15 17:12:58 -0500

    Create the pages for the index.php to links to

--- web/index.php
+++ web/index.php
@@ -13,4 +13,28 @@ $app->get('/', function () use ($app) {
     return $app['twig']->render('index.twig'); 
 });
 
+$app->get('/blog.html', function () use ($app) {
+    return $app['twig']->render('blog.twig'); 
+});
+
+$app->get('/music.html', function () use ($app) {
+    return $app['twig']->render('music.twig'); 
+});
+
+$app->get('/contactus.html', function () use ($app) {
+    return $app['twig']->render('contact.twig'); 
+});
+
+$app->get('/agile.html', function () use ($app) {
+    return $app['twig']->render('agile.twig'); 
+});
+
+$app->get('/photos.html', function () use ($app) {
+    return $app['twig']->render('photos.twig'); 
+});
+
+$app->get('/meetTheBand.html', function () use ($app) {
+    return $app['twig']->render('meetTheBand.twig'); 
+});
+
 $app->run(); 
