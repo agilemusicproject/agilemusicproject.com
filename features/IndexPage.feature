70731f38dc76a9c6dfb6a2f57083e49b5b488574
rawilder <rawilder0506@gmail.com>
2014-05-16 11:11:16 -0500

    made changes to make tests better and edited spacing of index.html and base.twig

--- features/IndexPage.feature
+++ features/IndexPage.feature
@@ -12,3 +12,4 @@ Scenario: Visit Index Page
   And there should be a link to "/music" called "Music"
   And there should be a link to "/photos" called "Photos"
   And there should be a link to "/agile" called "What is Agile?"
+  And there should be a link to "/dne" called "Doesnt Exist"
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -10,71 +10,67 @@ use Behat\Gherkin\Node\TableNode;
  */
 class FeatureContext implements SnippetAcceptingContext
 {
-    private $_driver;
-    private $_session;
+	private $_driver;
+	private $_session;
 
-    /**
+	/**
      * Initializes context.
      *
      * Every scenario gets it's own context object.
      * You can also pass arbitrary arguments to the context constructor through behat.yml.
      */
-    public function __construct()
-    {
-        $this->_driver = new \Behat\Mink\Driver\GoutteDriver();
-        $this->_session = new \Behat\Mink\Session($this->_driver);
-        $this->_session->start();        
-    }
+	public function __construct()
+	{
+		$this->_driver = new \Behat\Mink\Driver\GoutteDriver();
+		$this->_session = new \Behat\Mink\Session($this->_driver);
+		$this->_session->start();        
+	}
 
-   /**
+	/**
      * @Given I go to the AMP index page
      */
-    public function iGoToTheAmpIndexPage()
-    {
-        $this->_session->visit('http://amp.local/');
+	public function iGoToTheAmpIndexPage()
+	{
+		$this->_session->visit('http://amp.local/');
 
-    }
+	}
 
-     /**
+	/**
     * @Given there should be a link to :arg1 called :arg2
     */
-    public function thereShouldBeALinkToCalled($arg1, $arg2)
-    {
-      	$page = $this->_session->getPage();
-		$link = $page->findLink($arg2)->getAttribute('href');
+	public function thereShouldBeALinkToCalled($arg1, $arg2)
+	{
+		$page = $this->_session->getPage();
+		$link = $page->findLink($arg2);
 		if(is_null($link))
-			throw new Exception(
-					'Link not found called ' . $arg2
-				);
+			throw new Exception('Link not found called ' . $arg2);
 		else
-			if(strcmp($link, '$arg1')===0)
-				throw new Exception(
-						'Link found called ' . $arg2 . ' but does not go to ' . $arg1
-					);
-    }
+			if(strcmp($link->getAttribute('href'), '$arg1')===0)
+				throw new Exception('Link found called ' . $arg2 . ' but does not go to ' . $arg1);
+	}
 
-	
+
 	/**
      * @Then I should be on :arg1
      */
-    public function beOn($arg1)
-    {
-        //var_dump($this->_session->getCurrentUrl());
-        if (200 != $this->_session->getStatusCode()) {
-            throw new Exception(
-                'Status code was ' . $this->_session->getStatusCode()
-                . ' instead of 200.'
-            );
-        }
-		
+	public function beOn($arg1)
+	{
+		//var_dump($this->_session->getCurrentUrl());
+		if (200 != $this->_session->getStatusCode()) {
+			throw new Exception(
+				'Status code was ' . $this->_session->getStatusCode()
+				. ' instead of 200.'
+			);
+		}
+
 		// address testing
 		if (strpos($this->_session->getCurrentUrl(), $arg1) === false) {
 			throw new Exception(
 				'Address is incorrect: ' . $this->_session->getCurrentUrl()
 			);
 		}
-			
-    }
+
+	}
 
 }
 
