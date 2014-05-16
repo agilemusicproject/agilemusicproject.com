4c0df20260ac19489fe5dd1fc0ba86ec44fd60cd
chrisdlangham <chris.langham32@gmail.com>
2014-05-16 15:35:55 -0500

    added post-it graphics for the links on the index.html

---
+++ web/js/post-note.js
@@ -0,0 +1,17 @@
+var width = 610;
+var height = 458;
+
+function drawPostNote(idName, Xcoordinate, Ycoordinate, text)
+{
+  var canvas = document.getElementById( idName );
+  canvas.width = width/2;
+  canvas.height = height/2;
+  var context = canvas.getContext("2d");
+  var imageObj = new Image();
+  imageObj.onload = function(){
+    context.drawImage(imageObj, Xcoordinate, Ycoordinate, canvas.width, canvas.height);
+    context.font = "20pt Calibri";
+    context.fillText(text, canvas.width/3, canvas.height/3);
+  };
+  imageObj.src = "images/sticky-notes.bmp";
+}
