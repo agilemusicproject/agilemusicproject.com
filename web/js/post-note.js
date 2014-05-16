767b587e98204ccb3c66af2710c03fd81ea3234a
chrisdlangham <chris.langham32@gmail.com>
2014-05-16 16:51:32 -0500

    playing with canvas parameters in post-note.js

--- web/js/post-note.js
+++ web/js/post-note.js
@@ -10,8 +10,9 @@ function drawPostNote(idName, Xcoordinate, Ycoordinate, text)
   var imageObj = new Image();
   imageObj.onload = function(){
     context.drawImage(imageObj, Xcoordinate, Ycoordinate, canvas.width, canvas.height);
-    context.font = "10pt Calibri";
+    context.font = "20pt Calibri";
     context.fillText(text, canvas.width/3, canvas.height/3);
+    context.textAlign = 'center';
   };
   imageObj.src = "images/sticky-notes.bmp";
 }
