467f6f8821d7a6ad5097aed5929ee5aeb1bfc027
chrisdlangham <chris.langham32@gmail.com>
2014-05-19 11:18:33 -0500

    formatting for index.html

--- web/js/post-note.js
+++ web/js/post-note.js
@@ -2,15 +2,18 @@
 var width = 610;
 var height = 458;
 
-function drawPostNote(idName, Xcoordinate, Ycoordinate, text)
+function drawPostNote(idName, Xcoordinate, Ycoordinate, text, degrees)
 {
   var canvas = document.getElementById( idName );
   canvas.width = width/2;
   canvas.height = height/2;
   var context = canvas.getContext("2d");
   var imageObj = new Image();
-  imageObj.onload = function(){
-    context.drawImage(imageObj, Xcoordinate, Ycoordinate, canvas.width, canvas.height);
+  context.translate(canvas.width/2, canvas.height/2);
+  context.rotate(degrees * Math.PI/180);
+  context.translate(-canvas.width/2, -canvas.height/2);
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
     context.font = "20pt Calibri";
     context.fillText(text, canvas.width/3, canvas.height/3);
     context.textAlign = 'center';
