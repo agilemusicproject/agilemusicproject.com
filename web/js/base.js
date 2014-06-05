5c6903c0ba975a7dc3541eb89a91a62c909f2506
chrisdlangham <chris.langham32@gmail.com>
2014-06-05 11:25:36 -0500

    Added add band member button and put post-it under AMP logo

--- web/js/base.js
+++ web/js/base.js
@@ -1,17 +1,3 @@
-function visitMembersPage() {
-    window.location='http://amp.local/bandMembers';
-}
-
-function createContact() {
-    var name = document.getElementById('form_name').value;
-    var email = document.getElementById('form_email').value;
-    var message = document.getElementById('form_message').value;
-    
-    var newP = document.createElement('p');
-//    var newA = newP.appendChild('a');
-//    newA.Id = name.substr(0,5);
-//    newA.href = "mailto:" + email;
-    newP.className = "contactPageText";
-    newP.textContent = message;
-    document.getElementById('contactBand').appendChild(newP);
+function visitAddBandMembersPage() {
+    window.location='http://amp.local/meettheband/add';
 }
