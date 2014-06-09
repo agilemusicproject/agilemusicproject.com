ccdec37f61af2ad384d84ecf75a84b0bd6755ac1
chrisdlangham <chris.langham32@gmail.com>
2014-06-09 12:14:17 -0500

    meetTheBand page handles multiple paragraphs in bio

--- web/index.php
+++ web/index.php
@@ -58,6 +58,10 @@ $app->match('/meettheband', function (Request $request) use ($app) {
         $dao->delete($_POST['id']);
     }
     $results = $dao->getAll();
+    foreach ($results as &$person) {
+        $person['bio'] = explode("\n", $person['bio']);
+    }
+    unset($person);
     return $app['twig']->render('meetTheBand.twig', array('results' => $results));
 });
 
