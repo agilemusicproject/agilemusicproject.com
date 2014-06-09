dd3a54666b4fca517ff160cc42d81df566027e16
chrisdlangham <chris.langham32@gmail.com>
2014-06-09 09:20:08 -0500

    removing the rows and cols attr from the form generation

--- src/AMP/Form/MeetTheBandFormFactory.php
+++ src/AMP/Form/MeetTheBandFormFactory.php
@@ -20,18 +20,32 @@ class MeetTheBandFormFactory
         }
 
         $this->form = $formService->createBuilder('form', $data, array('csrf_protection' => false))
-        ->add('first_name', 'text', array('required' => true, 'label' => false,
-                                          'attr' => array('placeholder' => 'First Name')))
-        ->add('last_name', 'text', array('required' => true, 'label' => false,
-                                         'attr' => array('placeholder' => 'Last Name')))
-        ->add('roles', 'text', array('required' => true, 'label' => false,
-                                     'attr' => array('placeholder' => 'Roles')))
-        ->add('photo', 'file', array('required' => false))
-        ->add('bio', 'textarea', array('label' => false, 'label_attr' => array('style' => 'vertical-align: top;'),
-                                       'attr' => array('placeholder' => 'Bio',
-                                                       'cols' => '100', 'rows' => '20'), 'required' => false))
-        ->add('submit', 'submit')
-        ->getForm();
+            ->add('first_name', 'text', array(
+                'required' => true,
+                'label' => false,
+                'attr' => array('placeholder' => 'First Name'),
+            ))
+            ->add('last_name', 'text', array(
+                'required' => true,
+                'label' => false,
+                'attr' => array('placeholder' => 'Last Name'),
+            ))
+            ->add('roles', 'text', array(
+                'required' => true,
+                'label' => false,
+                'attr' => array('placeholder' => 'Roles'),
+            ))
+            ->add('photo', 'file', array(
+                'required' => false,
+            ))
+            ->add('bio', 'textarea', array(
+                'label' => false,
+                'label_attr' => array('style' => 'vertical-align: top;'),
+                'attr' => array('placeholder' => 'Bio', 'class' => 'meetBandTextArea'),
+                'required' => false
+            ))
+            ->add('submit', 'submit')
+            ->getForm();
     }
 
     public function getForm()
