68ac9be28bcd6bb68fa7d8590f6af401573020ff
U-Adam-Laptop\Adam <Adam@Adam-Laptop.(none)>
2014-06-16 12:01:33 -0500

    Added script to produce hash and changed how db test scripts run

--- test/scripts/db/create_test_db.sql
+++ test/scripts/db/create_test_db.sql
@@ -23,34 +23,6 @@ DROP DATABASE IF EXISTS `amp_test`;
 CREATE DATABASE IF NOT EXISTS `amp_test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `amp_test`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `band_members`
---
-
-CREATE TABLE IF NOT EXISTS `band_members` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` tinytext NOT NULL,
-  `last_name` tinytext NOT NULL,
-  `roles` tinytext NOT NULL,
-  `photo_filename` tinytext,
-  `bio` longtext,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(100) NOT NULL DEFAULT '',
-  `password` VARCHAR(255) NOT NULL DEFAULT '',
-  `roles` VARCHAR(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `users` (`username`, `password`, `roles`) VALUES ('admin', 
-			'5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg==', 'ROLE_ADMIN');
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
