eacd654b45e71dc2b8759e4ed529a84c15f05e78
chrisdlangham <chris.langham32@gmail.com>
2014-06-17 09:13:52 -0500

    formating for contact form

--- src/AMP/Form/MeetTheBandFormFactory.php
+++ src/AMP/Form/MeetTheBandFormFactory.php
@@ -14,28 +14,35 @@ class MeetTheBandFormFactory
         $default['photo_actions'] = 'photo_nothing';
         // research csrf_protection
         $this->form = $formService->createBuilder('form', $default)
-            ->add('first_name', 'text', array('required' => true,
-                                              'label' => false,
-                                              'attr' => array('placeholder' => 'First Name')))
-            ->add('last_name', 'text', array('required' => true,
-                                             'label' => false,
-                                             'attr' => array('placeholder' => 'Last Name')))
-            ->add('roles', 'text', array('required' => true,
-                                         'label' => false,
-                                         'attr' => array('placeholder' => 'Roles')))
+            ->add('first_name', 'text', array(
+                'required' => true,
+                'label' => false,
+                'attr' => array('placeholder' => 'First Name')))
+            ->add('last_name', 'text', array(
+                'required' => true,
+                'label' => false,
+                'attr' => array('placeholder' => 'Last Name')))
+            ->add('roles', 'text', array(
+                'required' => true,
+                'label' => false,
+                'attr' => array('placeholder' => 'Roles')))
             ->add('photo', 'file', array('required' => false))
-            ->add('photo_actions', 'choice', array('choices' => array('photo_nothing' => 'Do Nothing',
-                                                                     'photo_change' => 'New Photo',
-                                                                     'photo_delete' => 'Delete Photo'),
-                                                  'expanded' => 'false',
-                                                  'label' => 'Photo'))
-            ->add('photo', 'file', array('required' => false,
-                                         'label' => false))
+            ->add('photo_actions', 'choice', array(
+                'choices' => array(
+                    'photo_nothing' => 'Do Nothing',
+                    'photo_change' => 'New Photo',
+                    'photo_delete' => 'Delete Photo'),
+                'expanded' => 'false',
+                'label' => 'Photo'))
+            ->add('photo', 'file', array(
+                'required' => false,
+                'label' => false))
             // maybe put cols and rows in css
-            ->add('bio', 'textarea', array('label' => false,
-                                           'label_attr' => array('style' => 'vertical-align: top;'),
-                                           'attr' => array('placeholder' => 'Bio'),
-                                           'required' => false))
+            ->add('bio', 'textarea', array(
+                'label' => false,
+                'label_attr' => array('style' => 'vertical-align: top;'),
+                'attr' => array('placeholder' => 'Bio'),
+                'required' => false))
             ->add('submit', 'submit', array(
                 'attr' => array('class' => 'submitButton'),
             ))
