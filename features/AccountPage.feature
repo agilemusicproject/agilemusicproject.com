8d6a42df07c9cd2ac0d7b14d4b0b2e29d95ba4de
chrisdlangham <chris.langham32@gmail.com>
2014-06-20 12:16:08 -0500

    Account page feature and made defaultAction for AccountController

--- features/AccountPage.feature
+++ features/AccountPage.feature
@@ -3,7 +3,7 @@ Feature: AMP Web Site Account Page
   As a band member to the site
   I need to have an update account page
 
-  Scenario Outline: Change Band Member Password to test
+  Scenario Outline: Change Band Member Password
     When I go to "/account"
     Then I should be on "/login"
     When I fill in "_username" with "admin"
--- src/AMP/Controller/AccountController.php
+++ src/AMP/Controller/AccountController.php
@@ -12,25 +12,30 @@ class AccountController implements ControllerProviderInterface
         $controllers = $app['controllers_factory'];
 
         $controllers->match('', function (Request $request) use ($app) {
-            $formFactory = new \AMP\Form\UpdateAccountFormFactory($app['form.factory']);
-            $form = $formFactory->getForm();
-            $form->handleRequest($request);
-            if ($form->isValid()) {
-                $dao = new \AMP\Db\AccountManagerDAO($app['db']);
-                $data = $form->getData();
-                $data['username'] = $app['security']->getToken()->getUsername();
-                $data['newPassword'] = $app['security.encoder.digest']->encodePassword($data['newPassword'], '');
-                $data['oldPassword'] = $app['security.encoder.digest']->encodePassword($data['oldPassword'], '');
-                $data['confirmPassword'] = $app['security.encoder.digest']->encodePassword($data['confirmPassword'], '');
-                $data['currentPassword'] = $dao->getCurrentPassword($data);
-                if ($formFactory->isValidAuthentication($data)) {
-                    $dao->updateBandMemberPassword($data);
-                }
-                return $app->redirect('/');
-            }
-            return $app['twig']->render('updateAccount.twig', array('form' => $form->createView()));
+            return $this->defaultAction($request, $app);
         });
 
         return $controllers;
+    }
+
+    public function defaultAction(Request $request, Application $app)
+    {
+        $formFactory = new \AMP\Form\UpdateAccountFormFactory($app['form.factory']);
+        $form = $formFactory->getForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $dao = new \AMP\Db\AccountManagerDAO($app['db']);
+            $data = $form->getData();
+            $data['username'] = $app['security']->getToken()->getUsername();
+            $data['newPassword'] = $app['security.encoder.digest']->encodePassword($data['newPassword'], '');
+            $data['oldPassword'] = $app['security.encoder.digest']->encodePassword($data['oldPassword'], '');
+            $data['confirmPassword'] = $app['security.encoder.digest']->encodePassword($data['confirmPassword'], '');
+            $data['currentPassword'] = $dao->getCurrentPassword($data);
+            if ($formFactory->isValidAuthentication($data)) {
+                $dao->updateBandMemberPassword($data);
+            }
+            return $app->redirect('/');
+        }
+        return $app['twig']->render('updateAccount.twig', array('form' => $form->createView()));
     }
 }
