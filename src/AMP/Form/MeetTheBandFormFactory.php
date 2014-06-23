90cac52c6dff228282f481fe8d08cb0ea6c67ad4
chrisdlangham <chris.langham32@gmail.com>
2014-06-23 08:29:52 -0500

    changed required attribute into Assert NotBlank in formfactory classes

--- src/AMP/Form/MeetTheBandFormFactory.php
+++ src/AMP/Form/MeetTheBandFormFactory.php
@@ -12,41 +12,41 @@ class MeetTheBandFormFactory extends BaseFormFactory
         $default['photo_actions'] = 'photo_nothing';
         $this->form = $formService->createBuilder('form', $default)
             ->add('first_name', 'text', array(
-                                              'required' => true,
-                                              'label' => false,
-                                              'attr' => array('placeholder' => 'First Name'),
-                                              'label_attr' => array('class' => 'formLabel'),
+                'constraints' => new Assert\NotBlank(),
+                'label' => false,
+                'attr' => array('placeholder' => 'First Name'),
+                'label_attr' => array('class' => 'formLabel'),
             ))
             ->add('last_name', 'text', array(
-                                             'required' => true,
-                                             'label' => false,
-                                             'attr' => array('placeholder' => 'Last Name'),
-                                             'label_attr' => array('class' => 'formLabel'),
+                'constraints' => new Assert\NotBlank(),
+                'label' => false,
+                'attr' => array('placeholder' => 'Last Name'),
+                'label_attr' => array('class' => 'formLabel'),
             ))
             ->add('roles', 'text', array(
-                                         'required' => true,
-                                         'label' => false,
-                                         'attr' => array('placeholder' => 'Roles'),
-                                         'label_attr' => array('class' => 'formLabel'),
+                'constraints' => new Assert\NotBlank(),
+                'label' => false,
+                'attr' => array('placeholder' => 'Roles'),
+                'label_attr' => array('class' => 'formLabel'),
             ))
             ->add('photo_actions', 'choice', array(
-                                                   'choices' => array(
-                                                                      'photo_nothing' => 'Do Nothing',
-                                                                      'photo_change' => 'New Photo',
-                                                                      'photo_delete' => 'Delete Photo'),
-                                                   'expanded' => false,
-                                                   'label' => 'Photo',
+                'choices' => array(
+                    'photo_nothing' => 'Do Nothing',
+                    'photo_change' => 'New Photo',
+                    'photo_delete' => 'Delete Photo'),
+                'expanded' => false,
+                'label' => 'Photo',
             ))
             ->add('photo', 'file', array(
-                                         'required' => false,
-                                         'label' => false,
+                'required' => false,
+                'label' => false,
             ))
             ->add('bio', 'textarea', array(
-                                           'label' => false,
-                                           'label_attr' => array(
-                                                                 'class' => 'formLabel'),
-                                           'attr' => array('placeholder' => 'Bio'),
-                                           'required' => false,
+                'label' => false,
+                'label_attr' => array(
+                    'class' => 'formLabel'),
+                'attr' => array('placeholder' => 'Bio'),
+                'required' => false,
             ))
             ->add('submit', 'submit')
             ->getForm();
--- src/AMP/Form/UpdateAccountFormFactory.php
+++ src/AMP/Form/UpdateAccountFormFactory.php
@@ -9,15 +9,18 @@ class UpdateAccountFormFactory extends BaseFormFactory
     public function __construct(FormFactory $formService)
     {
         $this->form = $formService->createBuilder('form')
-            ->add('oldPassword', 'password', array('constraints' => new Assert\NotBlank(),
-                                                   'label' => 'Old Password:',
-                                                   'label_attr' => array('class' => 'formLabel')))
-            ->add('newPassword', 'password', array('constraints' => new Assert\NotBlank(),
-                                                   'label' => 'New Password:',
-                                                   'label_attr' => array('class' => 'formLabel')))
-            ->add('confirmPassword', 'password', array('constraints' => new Assert\NotBlank(),
-                                                   'label' => 'Confirm Password:',
-                                                   'label_attr' => array('class' => 'formLabel')))
+            ->add('oldPassword', 'password', array(
+                'constraints' => new Assert\NotBlank(),
+                'label' => 'Old Password:',
+                'label_attr' => array('class' => 'formLabel')))
+            ->add('newPassword', 'password', array(
+                'constraints' => new Assert\NotBlank(),
+                'label' => 'New Password:',
+                'label_attr' => array('class' => 'formLabel')))
+            ->add('confirmPassword', 'password', array(
+                'constraints' => new Assert\NotBlank(),
+                'label' => 'Confirm Password:',
+                'label_attr' => array('class' => 'formLabel')))
             ->add('update', 'submit')
             ->getForm();
     }
