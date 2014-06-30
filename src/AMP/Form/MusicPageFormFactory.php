1548e25407fe3959824a27a3b1521085e42bceb5
chrisdlangham <chris.langham32@gmail.com>
2014-06-30 12:28:40 -0500

    took out rich text editor from music edit form

--- src/AMP/Form/MusicPageFormFactory.php
+++ src/AMP/Form/MusicPageFormFactory.php
@@ -9,14 +9,14 @@ class MusicPageFormFactory extends BaseFormFactory
     public function __construct(FormFactory $formService, array $default = null, $isUpdateForm = false)
     {
         $this->formBuilder = $formService->createBuilder('form', $default)
-            ->add('embed', 'textarea', array(
-                'label' => 'Click the source button then copy and paste the embed code from soundcloud',
-                'label_attr' => array('class' => 'formLabel'),
-                'constraints' => new Assert\NotBlank(),
-            ))
             ->add('song_order', 'text', array(
                 'label_attr' => array('class' => 'formLabel'),
                 'attr' => array('pattern' => '[0-9]'),
+                'constraints' => new Assert\NotBlank(),
+            ))
+            ->add('embed', 'textarea', array(
+                'label' => 'Copy and paste the embed code from soundcloud here',
+                'label_attr' => array('class' => 'formLabel'),
                 'constraints' => new Assert\NotBlank(),
             ))
             ->add('submit', 'submit');
