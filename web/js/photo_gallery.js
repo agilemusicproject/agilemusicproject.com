9bec989a679902e948053c90eaa9439d24502ce4
Adam Wilder <rawilder0506@gmail.com>
2014-07-02 16:44:59 -0500

    new gallery again! fancybox

--- web/js/photo_gallery.js
+++ web/js/photo_gallery.js
@@ -1,27 +1,22 @@
-//Reference http://www.jssor.com/development/slider-with-caption.html
-//Reference http://www.jssor.com/development/reference-ui-definition.html#captiondefinition
-//Reference http://www.jssor.com/development/tool-caption-transition-viewer.html
+$(document).ready(function() {
+    //Enable thumbnail helper and set custom options
+    $(".fancybox").fancybox({
+        beforeLoad: function() {
+            var el, id = $(this.element).data('title-id');
 
-jQuery(document).ready(function ($) {
-    var _CaptionTransitions = [];
-    _CaptionTransitions["T"] = { $Duration: 800, $FlyDirection: 4, $Easing: $JssorEasing$.$EaseInCubic };
-    _CaptionTransitions["B"] = { $Duration: 800, $FlyDirection: 8, $Easing: $JssorEasing$.$EaseInCubic };
-    var options = {
-        $FillMode: 1,
-        $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-        $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
-        $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
-            $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
-            $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
-            $PlayInMode: 3,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
-            $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
+            if (id) {
+                el = $('#' + id);
+            
+                if (el.length) {
+                    this.title = el.html();
+                }
+            }
         },
-        $ArrowNavigatorOptions: {                       //[Optional] Options to specify and enable arrow navigator or not
-            $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
-            $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-            $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-            $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+        helpers:  {
+            thumbs : {
+                width: 50,
+                height: 50
+            }
         }
-    };
-    var jssor_slider1 = new $JssorSlider$("photo_gallery", options);
+    });
 });
