8334d67aa61fa5b1dd95d97e370d98525b80ecb0
Adam Wilder <rawilder0506@gmail.com>
2014-07-14 10:11:47 -0500

    Lined paper is now made with css rather than using an image, and the blog text lines up with the lines but date and category don't all the time

---
+++ web/blog/wp-content/themes/responsive-child/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Footer Template
+ *
+ *
+ * @file           footer.php
+ * @package        Responsive
+ * @author         Emil Uzelac
+ * @copyright      2003 - 2014 CyberChimps
+ * @license        license.txt
+ * @version        Release: 1.2
+ * @filesource     wp-content/themes/responsive/footer.php
+ * @link           http://codex.wordpress.org/Theme_Development#Footer_.28footer.php.29
+ * @since          available since Release 1.0
+ */
+
