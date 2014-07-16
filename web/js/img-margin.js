52bd3055799256fb8712223ecd8d7c9a79e1547e
Adam Wilder <rawilder0506@gmail.com>
2014-07-16 13:40:31 -0500

    Changed blog post img margin to be properly computed

--- web/js/img-margin.js
+++ web/js/img-margin.js
@@ -3,7 +3,7 @@
        $(".post-entry img").each(function() {
            marginBottom = parseInt($(this).css("marginBottom"), 10);
            remainder = parseInt($(this).outerHeight(true), 10) % 20;
-           $(this).css("marginBottom", marginBottom+remainder+"px");
+           $(this).css("marginBottom", marginBottom-remainder+"px");
        });
     });
 }(jQuery));
