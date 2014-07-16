4840eac97dd959b4039b19ecfeee99030ec5f53b
Adam Wilder <rawilder0506@gmail.com>
2014-07-16 10:01:28 -0500

    Added javascript that makes images inside wordpress post be divisible by 20 to allow text to line up with lines

---
+++ web/js/img-margin.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(document).ready(function() {
+       $(".post-entry img").each(function() {
+           marginBottom = parseInt($(this).css("marginBottom"), 10);
+           remainder = parseInt($(this).outerHeight(true), 10) % 20;
+           $(this).css("marginBottom", marginBottom+remainder+"px");
+       });
+    });
+}(jQuery));
