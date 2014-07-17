5414aad8255ab3aff292ea43504d217d1cbd076c
chrisdlangham <chris.langham32@gmail.com>
2014-07-17 11:32:29 -0500

    edited contact-us.js

--- web/js/contact-us.js
+++ web/js/contact-us.js
@@ -1,1 +1,7 @@
-$('div#form').reset();
+$(document).ready(function() {
+if ( $('#emailNotification').attr('value') == "1" ) {
+      console.log('true');
+      $("#contactForm")[0].reset();
+    }
+console.log('false');
+});
