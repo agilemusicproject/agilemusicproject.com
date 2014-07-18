403984f5d17a5f303e8ed75e482a6fbf349815bd
chrisdlangham <chris.langham32@gmail.com>
2014-07-18 15:39:57 -0500

    kinda works with two buttons but disappears

--- web/js/delete-confirm.js
+++ web/js/delete-confirm.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
-  $(".deleteButton").click(function(){
-    window.alert = function(message, fallback){
+  window.alert = function(message, fallback){
     if(fallback)
     {
         old_alert(message);
         return;
     }
     $(document.createElement('div'))
-        .attr({title: 'Alert', 'class': 'alert'})
-        .html(message)
-        .dialog({
-            buttons: {OK: function(){$(this).dialog('close');}},
-            close: function(){$(this).remove();},
-            draggable: true,
-            modal: true,
-            resizable: false,
-            width: 'auto'
-        });
-};
-  });
+    .attr({title: 'Alert', 'class': 'alert'})
+    .html(message)
+    .dialog({
+      buttons: [
+        {text: "Yes", click: function(){$(this).dialog('close');}},
+        {text: "No", click: function(){$(this).dialog('close');}}
+      ],
+      close: function(){$(this).remove();},
+      draggable: true,
+      modal: true,
+      resizable: false,
+      width: 'auto'
+    });
+  };
 });
