113a4bff0682d1e355863dbcae6dba29c93783d7
chrisdlangham <chris.langham32@gmail.com>
2014-07-21 10:44:19 -0500

    edited twigs to the new format for delete-confirm.js

--- features/MusicPage.feature
+++ features/MusicPage.feature
@@ -43,9 +43,9 @@ Feature: AMP Web Site Music Page
     And I should see 2 "iframe" elements
     And I should see 6 ".dots" elements
     And I should see 2 ".deleteButton" elements
-    When I press "Delete"
     And I press "Cancel Delete"
-    Then I should not see an ".dialogBox" element
+    When I press "Delete"
+    Then I should not see an ".cancelButton" element
     And I should see an ".deleteButton" element
     When I press "Delete"
     And I press "Confirm Delete"
--- web/js/delete-confirm.js
+++ web/js/delete-confirm.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   $(".deleteButton").click(function() {
-    $(this).siblings(".cancelButton")[0].style.display = 'block';
-    $(this).siblings(".confirmButton")[0].style.display = 'block';
+    $(this).siblings(".cancelButton")[0].style.display = 'inline-block';
+    $(this).siblings(".confirmButton")[0].style.display = 'inline-block';
     $(this)[0].style.display = 'none';
   });
 
   $(".cancelButton").click(function() {
     $(this)[0].style.display = 'none';
     $(this).parent().children(".confirmButton")[0].style.display = 'none';
-    $(this).parent().children(".deleteButton")[0].style.display = 'block';
+    $(this).parent().children(".deleteButton")[0].style.display = 'inline-block';
   });
 });
 
