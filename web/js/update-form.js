eee8cb14ff38743d407fa04837fa746d41d2d24c
chrisdlangham <chris.langham32@gmail.com>
2014-07-24 16:21:31 -0500

    better way to handle duplicate files

--- web/js/update-form.js
+++ web/js/update-form.js
@@ -1,15 +1,16 @@
 function duplicateFileError(filename) {
     $.ajax({
-            url: "/images/photos/" . filename,
-            success: function() {
+        url: "/images/photos/" . filename,
+        success: function(data) {
+            if ($(data).find("img")[0].exists()) {
                 $("#form_photo").after("<div id=\"duplicateError\">There is already a file with this name.</div>");
                 $("#form_photo_rename").css("display","block");
-            },
-            error: function() {
+            } else {
                 $("#form_submit").removeAttr("disabled");
                 $("#duplicateError").remove();
             }
-        });
+        }
+    });
 }
 $(document).ready(function() {
     $("#form_photo_actions").change(function() {
