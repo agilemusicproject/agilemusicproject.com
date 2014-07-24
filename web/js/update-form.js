f03f2e653145ff084e5bf54d7880dd0415db533d
chrisdlangham <chris.langham32@gmail.com>
2014-07-24 15:46:58 -0500

    ajax detects if file name is already used

--- web/js/update-form.js
+++ web/js/update-form.js
@@ -3,6 +3,7 @@ function duplicateFileError() {
 }
 $(document).ready(function() {
     $("#form_photo_actions").change(function() {
+        $("#form_photo_rename").css("display","none");
         var choice = $(this).val();
         if (choice == "photo_file") {
             $("#form_photo")[0].style.display='block';
@@ -16,11 +17,16 @@ $(document).ready(function() {
         }
     });
     $("#form_photo").change(function() {
+        $("#form_submit").attr("disabled", "disabled");
         var filename = $(this)[0].files[0].name;
         $.ajax({
             url: "/images/photos/" . filename,
-            success: function(){
+            success: function() {
+                $("#form_photo").after("<div>There is already a file with this name.</div>");
                 $("#form_photo_rename").css("display","block");
+            },
+            error: function() {
+                $("#form_submit").removeAttr("disabled");
             }
         });
     });
