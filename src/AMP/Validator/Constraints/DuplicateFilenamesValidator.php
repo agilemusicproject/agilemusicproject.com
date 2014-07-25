da640d85a1a62dfbfabda16bba0d342185556ec5
Adam Wilder <rawilder0506@gmail.com>
2014-07-25 14:21:13 -0500

    fixed some code review issues regarding duplicatefilenamesvalidator

--- src/AMP/Validator/Constraints/DuplicateFilenamesValidator.php
+++ src/AMP/Validator/Constraints/DuplicateFilenamesValidator.php
@@ -1,8 +1,10 @@
 <?php
 namespace AMP\Validator\Constraints;
 
+use AMP\UploadManager;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @Annotation
@@ -11,18 +13,41 @@ class DuplicateFilenamesValidator extends ConstraintValidator
 {
     public function validate($file, Constraint $constraint)
     {
+        $isValid = true;
+        
         if (!is_null($file)) {
+            $uploadManager = $constraint->uploadManager;
             if (is_object($file)) {
-                $filename = $constraint->uploadManager->getFilePath($file->getClientOriginalName());
-                if (file_exists($filename)) {
-                    $this->context->addViolation($constraint->message);
-                }
+                $isValid = $this->isValidUploadedFile($file, $uploadManager);
             } else {
-                $filename = basename($file);
-                if (file_exists($constraint->uploadManager->getFilePath($filename))) {
-                    $this->context->addViolation($constraint->message);
-                }
+                $isValid = $this->isValidUploadByURL($file, $uploadManager);
             }
         }
+        
+        if (!$isValid) {
+            $this->context->addViolation($constraint->message);
+        }
+    }
+    
+    private function isValidUploadedFile(UploadedFile $file, UploadManager $uploadManager)
+    {
+        $filepath = $uploadManager->getFilePath($file->getClientOriginalName());
+        return $this->isValidFilePath($filepath);
+    }
+    
+    private function isValidUploadByURL($file, UploadManager $uploadManager)
+    {
+        $filename = basename($file);
+        $filepath = $uploadManager->getFilePath($filename);
+        return $this->isValidFilePath($filepath);
+    }
+    
+    private function isValidFilePath($filepath)
+    {
+        if (file_exists($filepath)) {
+           return false;
+        }
+        
+        return true;
     }
 }
--- web/js/update-form.js
+++ web/js/update-form.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
         var choice = $(this).val();
         if (choice == "photo_file") {
             $("#form_photo")[0].style.display='block';
+            $("#form_photo").prop('required', true);
             $("#form_photo_url")[0].style.display='none';
+            $("#form_photo_url").prop('required', false);
         } else if (choice == "photo_url") {
             $("#form_photo")[0].style.display='none';
+            $("#form_photo").prop('required', false);
             $("#form_photo_url")[0].style.display='block';
+            $("#form_photo_url").prop('required', true);
         } else {
             $("#form_photo")[0].style.display='none';
+            $("#form_photo").prop('required', false);
             $("#form_photo_url")[0].style.display='none';
+            $("#form_photo_url").prop('required', false);
         }
     });
 });
