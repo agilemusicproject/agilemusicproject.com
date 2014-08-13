3a161ad2a4d4c83880625f7648bf9dfdaa239b99
Adam Wilder <rawilder0506@gmail.com>
2014-08-13 10:57:22 -0500

    changed preview to be removed when choosing a different option for band member photos

--- web/js/preview-photo.js
+++ web/js/preview-photo.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             reader.onload = function (e) {
                 $('.preview').css("display", "inline").attr('src', e.target.result);
                 $("#newPreviewLabel").css("display", "inline");
-            }
+            };
             reader.readAsDataURL(this.files[0]);
         }
     });
@@ -14,19 +14,21 @@ $(document).ready(function() {
         var url = $('#form_photo_url').val();
         if (url != "") {
             $('#form_photo').val('');
-            $('.preview').css("inline", "block").attr('src', url);
+            $('.preview').css("display", "inline").attr('src', url);
             $("#newPreviewLabel").css("display", "inline");
         }
     });
     $(".preview").error(function() {
+        $('#form_photo').val('');
+        $('#form_photo_url').val('');
         $(this).css("display", "none");
         $("#newPreviewLabel").css("display", "none");
     });
     $("#form_photo_actions").change(function() {
         var option = $("#form_photo_actions option:selected").text();
-        if (option == "Add Nothing" || option == "Do Nothing" || option == "Delete Photo") {
-            $(".preview").css("display", "none");
-            $("#newPreviewLabel").css("display", "none");
-        }
+        $('#form_photo').val('');
+        $('#form_photo_url').val('');
+        $(".preview").css("display", "none");
+        $("#newPreviewLabel").css("display", "none");
     });
 });
