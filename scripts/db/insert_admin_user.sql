5b448ca4caf0aa3f992f73df60a890b43313cb54
Adam Wilder <rawilder0506@gmail.com>
2014-09-11 10:42:11 -0500

    Added script for inserting admin account into AMP users table

---
+++ scripts/db/insert_admin_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`username`, `password`, `roles`) VALUES ('admin', 
+			'b9IWS/xWSlKE9VHIKen7N32HISArjgywjBU0jAbcX2q4aVFVOyixY0m2NrAl81/NnlvANZpuGJK/a/YCXP0HtQ==', 'ROLE_ADMIN');
