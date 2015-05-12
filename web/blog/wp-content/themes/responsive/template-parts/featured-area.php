2b58799c886728d5211190b9418160b4217f6e00
Michael Riggs <secretcxman@gmail.com>
2015-05-12 14:48:57 -0500

    Wordpress upgrade.

--- web/blog/wp-content/themes/responsive/template-parts/featured-area.php
+++ web/blog/wp-content/themes/responsive/template-parts/featured-area.php
@@ -1,7 +1,7 @@
 <?php
 
 // Exit if accessed directly
-if( !defined( 'ABSPATH' ) ) {
+if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -20,7 +20,7 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 
 		<h1 class="featured-title">
 			<?php
-			if( isset( $responsive_options['home_headline'] ) && $db && $empty )
+			if ( isset( $responsive_options['home_headline'] ) && $db && $empty )
 				echo $responsive_options['home_headline'];
 			else {
 				_e( 'Hello, World!', 'responsive' );
@@ -30,7 +30,7 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 
 		<h2 class="featured-subtitle">
 			<?php
-			if( isset( $responsive_options['home_subheadline'] ) && $db && $empty )
+			if ( isset( $responsive_options['home_subheadline'] ) && $db && $empty )
 				echo $responsive_options['home_subheadline'];
 			else
 				_e( 'Your H2 subheadline here', 'responsive' );
@@ -45,13 +45,13 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 		} ?>
 
 
-		<?php if( $responsive_options['cta_button'] == 0 ): ?>
+		<?php if ( $responsive_options['cta_button'] == 0 ): ?>
 
 			<div class="call-to-action">
 
 				<a href="<?php echo $responsive_options['cta_url']; ?>" class="blue button">
 					<?php
-					if( isset( $responsive_options['cta_text'] ) && $db && $emtpy_cta )
+					if ( isset( $responsive_options['cta_text'] ) && $db && $emtpy_cta )
 						echo $responsive_options['cta_text'];
 					else
 						_e( 'Call to Action', 'responsive' );
@@ -62,8 +62,7 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 
 		<?php endif; ?>
 
-	</div>
-	<!-- end of .col-460 -->
+	</div><!-- end of .col-460 -->
 
 	<div id="featured-image" class="grid col-460 fit">
 
@@ -71,7 +70,6 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 
 		<?php echo do_shortcode( wpautop( $featured_content ) ); ?>
 
-	</div>
-	<!-- end of #featured-image -->
+	</div><!-- end of #featured-image -->
 
 </div><!-- end of #featured -->
